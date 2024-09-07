ecc87e9e136a8176193dec12afdd2be054b0568b
Luna <69774917+Lluna4@users.noreply.github.com>
2024-09-07 03:05:52 +0200

    Add files via upload

---
+++ test_client.cpp
@@ -0,0 +1,65 @@
+#include "comp_time_write.hpp"
+#include "netlib.h"
+#include <print>
+#include <iostream>
+#include <chrono>
+
+std::pair<char *, size_t> load(char *path)
+{
+	FILE* f = fopen(path, "rb");
+	fseek(f, 0, SEEK_END);
+	size_t file_size = ftell(f);
+	rewind(f);
+	char *buffer = (char *)malloc((file_size + 1) * sizeof(char));
+	fread(buffer, sizeof(uint8_t), file_size, f);
+
+	return std::pair<char *, size_t> {buffer, file_size};
+}
+
+int main()
+{
+	using clock = std::chrono::system_clock;
+	using ms = std::chrono::duration<double, std::milli>;
+
+	char *buffer = (char *)calloc(4096, sizeof(char));
+	char *start_buffer = buffer;
+	int sockfd = netlib::connect_to_server("127.0.0.1", 8000);
+
+	array_with_size<(4096 - 12), char *> name;
+	name.array = "a.msi";
+	std::tuple<int,int, array_with_size<(4096 - 12), char *>, int> file_name = {4088, 1, name, 10};
+	constexpr std::size_t size = std::tuple_size_v<decltype(file_name)>;
+	write_comp_pkt(size, buffer, file_name);
+	send(sockfd, start_buffer, 4096, 0);
+
+	std::pair<char *, size_t> ret = load("../cmake-build-debug/a.msi");
+
+	char *buf = ret.first;
+	size_t file_size = ret.second;
+	std::println("Start!");
+	const auto before1 = clock::now();
+	while (true)
+	{
+		buffer = start_buffer;
+		memset(buffer, 0, 4096);
+		int send_size = 4088;
+
+		if (file_size < 4088)
+			send_size = file_size;
+
+		array_with_size<4088, const char *> data;
+		data.array = buf;
+		std::tuple<int, int, array_with_size<4088, const char*>> data_pkt = {4088, 2, data};
+		constexpr std::size_t size_ = std::tuple_size_v<decltype(data_pkt)>;
+		write_comp_pkt(size_, buffer, data_pkt);
+		send(sockfd, start_buffer, 4096, 0);
+
+		if (send_size < 4088)
+			break;
+		file_size -= 4088;
+	}
+	const ms duration1 = clock::now() - before1;
+	std::cout << "It took " << duration1.count() << "ms" << std::endl;
+	std::println("Done!");
+	return  0;
+}
